f814d09b3a4fd8cdbac3f63385af844243c06228
James <james@hawkinsplace.co.uk>
2021-04-16 10:58:07 +0100

    added create_bow script

--- create_bow_corpus.py
+++ create_bow_corpus.py
@@ -1,24 +1,20 @@
 import pandas as pd
-from time import time
-
 from nltk.stem import WordNetLemmatizer
-from gensim.parsing.preprocessing import STOPWORDS
 import gensim
+from time import time
+import sys
 
 DATASET = "amazon_reviews_us_Grocery_v1_00.tsv"
+
+n_topics = sys.argv[1]
 
 rtime = time()
 
 print("loading dataset...")
 ds = pd.read_table(DATASET, error_bad_lines=False, header=0, warn_bad_lines=False)
-print("done!")
-
 # Drop null rows
 ds = ds.dropna()
-
-lda = gensim.models.LdaModel.load('model/lda.model')
-dct = gensim.corpora.Dictionary.load('model/lda.model.id2word')
-
+print("done!")
 lemmatizer = WordNetLemmatizer()
 
 def preprocess(text):
@@ -30,22 +26,18 @@ def preprocess(text):
 
 
 reviews = ds["review_body"]
-less_reviews = reviews[:100]
+less_reviews = reviews
 processed = []
 for review in less_reviews:
     processed.append(preprocess(review))
 
-#dictionary = gensim.corpora.Dictionary(processed) # construct word<->id mappings - it does it in alphabetical order
+dct = gensim.corpora.Dictionary.load(f'model{n_topics}/lda.model.id2word')
 
 bow_corpus = [dct.doc2bow(review) for review in processed]
-print(bow_corpus)
-f=open("bow_corpus.txt","w+")
+
+f=open(f"model{n_topics}/bow_corpus.txt","w+")
 f.write(str(bow_corpus))
 f.close()
-print(dct)
-for idx, topic in lda.print_topics(-1): # The words occuring in each class, and the weight given for that
-    print("Topic: {} \nWords: {}".format(idx, topic ))
-    print("\n")
 
 rtime = time() - rtime
 print("The program ran in", rtime, "seconds")
