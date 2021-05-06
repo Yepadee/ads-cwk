aa424a8563470ca912e17c3ce85bd8835421a6ca
James <james@hawkinsplace.co.uk>
2021-05-06 18:47:40 +0000

    save

--- topic-modelling.py
+++ topic-modelling.py
@@ -29,7 +29,7 @@ def preprocess(text):
     return result
 
 
-reviews = ds["review_body"]
+reviews = ds["review_headline"]
 less_reviews = reviews
 processed = []
 for review in less_reviews:
@@ -38,13 +38,18 @@ for review in less_reviews:
 dictionary = gensim.corpora.Dictionary(processed) # construct word<->id mappings - it does it in alphabetical order
 
 bow_corpus = [dictionary.doc2bow(review) for review in processed]
+f=open(f"h_model{n_topics}/bow_corpus.txt","w+")
+f.write(str(bow_corpus))
+f.close()
+
+rtime = time() - rtime
 
 lda_model =  gensim.models.LdaModel(bow_corpus, 
                                    num_topics=n_topics, 
                                    id2word=dictionary,                                    
                                    passes=10,
                                    ) 
-lda_model.save(f'model{n_topics}/lda.model')
+lda_model.save(f'h_model{n_topics}/lda.model')
 
 rtime = time() - rtime
 print("The program ran in", rtime, "seconds")
